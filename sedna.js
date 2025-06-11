7ecef89b9f8185716ea33a12ae958714ec77619f
João Iacillo <joao@iacillo.dev.br>
2025-06-11 01:57:37 -0300

    Improve Sedna
    Scenes and characters can be created directly through the story
    config. The story now has a data property. Characters speaks are
    like Ren'py now, being provided to the scenes as arguments.
    Added scene call function and menus are now created by returning
    an object from the scene functions.

--- sedna.js
+++ sedna.js
@@ -1,272 +1,303 @@
-(function () {
+/**
+ * A character in the story.
+ * Can be used to represent the player or any other entity.
+ * The narrator is also a character.
+ */
+class SednaCharacter {
   /**
-   * Base class for characters in the game.
-   * Characters can be used to represent players, NPCs, or any other entities in the game.
+   * @param {Sedna} story - The story this character belongs to.
+   * @param {string} id - Character identifier (usually the first letter of the name)
+   * @param {string} [name="???"] - Name of the character.
+   * @param {Record<string, unknown>} [data={}] - Additional data associated with the character.
    */
-  class Character {
-    /**
-     * @param {Game} game - The game instance this character belongs to.
-     * @param {string} id - Unique identifier for the character.
-     * @param {string} [name="???"] - Name of the character.
-     * @param {Object} [data={}] - Additional data associated with the character.
-     */
-    constructor(game, id, name = "???", data = {}) {
-      this.game = game;
-      this.id = id;
-      this.name = name;
-      this.data = data;
-    }
+  constructor(story, id, name = "???", data = {}) {
+    this.story = story;
+    this.id = id;
+    this.name = name;
+    this.data = data;
 
-    /**
-     * Sends a message from this character.
-     * @param {string} message - The message to send.
-     * @returns {any} Whichever value the current renderer onMessage method returns.
-     */
-    say(message) {
-      return this.game.renderer.onMessage?.(this, message);
-    }
-
-    toString() {
-      return this.name;
-    }
+    this.functionLike = null;
   }
 
   /**
-   * Base class for game renderers.
-   * This class should be extended to create custom renderers.
+   * Returns this character's say function clone, bound to this character
+   * information.
+   *
+   * Auto-completion is something that doesn't work when using this,
+   * but it shouldn't be necessary due to the Character simple nature.
+   *
+   * Props:
+   *   - `.data` - Character data
+   *   - `.character` - Character reference
+   *   - `.toString()` - Returns this character name
    */
-  class Renderer {
-    constructor() {
-      this.game = null;
-    }
-    
-    insert(game) {
-      this.game = game;
-    }
+  asFunction() {
+    if (this.functionLike) return this.functionLike;
+    this.functionLike = this.say.bind(this);
+    this.functionLike.toString = this.toString.bind(this);
+    this.functionLike.data = this.data;
+    this.functionLike.character = this;
+    return this.functionLike;
   }
 
   /**
-   * Customizable HTML renderer. It's the default for the game.
+   * Sends a message from this character.
+   * @returns {unknown} Whichever value the current renderer onMessage method returns.
    */
-  class HtmlRenderer extends Renderer {
-    /**
-     * 
-     * @param {Object} config - Configuration object.
-     * @param {HTMLElement?} config.container - The container element where the game will be rendered.
-     * @param {Object} config.narrator - Configuration for the narrator.
-     * @param {boolean} config.narrator.treatAsCharacter - If true, the narrator will be treated as a normal character.
-     * @param {boolean} config.narrator.italicize - If true, the narrator's messages will be italicized with <em>.
-     * @param {Object} config.classes - CSS classes to apply to the game elements.
-     * @param {string} config.classes.name - Class for character names.
-     * @param {string} config.classes.message - Class for messages.
-     * @param {string} config.classes.menu - Class for menu elements.
-     * @param {string} config.classes.button - Class for menu buttons.
-     * @param {string} config.classes.narrator - Class for the narrator messages.
-     */
-    constructor(config = {}) {
-      super();
-      
-      this.container = config.container || document.body;
+  say(message, templates) {
+    return this.story.renderer.onMessage?.(this, String.raw(message, templates));
+  }
 
-      this.narrator = {
-        treatAsCharacter: false,
-        italicize: true,
-        ...config.narrator
-      };
+  toString() {
+    return this.name;
+  }
+}
 
-      this.classes = {
-        name: "game-character-name",
-        message: "game-message",
-        menu: "game-menu",
-        button: "game-menu-button",
-        narrator: "game-narrator-message",
-        ...config.classes
-      }
-    }
+/**
+ * Base class for renderers.
+ * Does nothing by default. Extend it to create a custom renderer or use the
+ * builtin ones:
+ * - {@link SednaHTMLRenderer}
+ */
+class SednaRenderer {
+  constructor() {
+    this.story = null;
+  }
+
+  /**
+   * @param {Sedna} story
+   */
+  setStory(story) {
+    this.story = story;
+  }
+}
+
+/**
+ * @typedef {object} SednaHTMLRendererConfig
+ * @prop {HTMLElement | undefined} container - The container element where the game will be rendered.
+ * @prop {object} narratorDisplay - Configuration for the narrator.
+ * @prop {boolean} narratorDisplay.treatAsCharacter - If true, the narrator will be treated as a normal character.
+ * @prop {boolean} narratorDisplay.italicize - If true, the narrator's messages will be italicized with <em>.
+ * @prop {object} classes - CSS classes to apply to the game elements.
+ * @prop {string} classes.name - Class for character names.
+ * @prop {string} classes.message - Class for messages.
+ * @prop {string} classes.menu - Class for menu elements.
+ * @prop {string} classes.button - Class for menu buttons.
+ * @prop {string} classes.narrator - Class for the narrator messages.
+ */
+
+/**
+ * Renders story as customizable HTML elements.
+ */
+class SednaHTMLRenderer extends SednaRenderer {
+  /**
+   * @param {SednaHTMLRendererConfig} config
+   */
+  constructor(config = {}) {
+    super();
     
-    onNarratorMessage(message) {
-      const messageEl = document.createElement("div");
-      messageEl.className = this.classes.narrator;
-      
-      if (this.narrator.italicize) {
-        const italicEl = document.createElement("em");
-        italicEl.innerText = message;
-        messageEl.appendChild(italicEl);
-      } else {
-        messageEl.innerText = message;
-      }
+    this.container = config.container || document.body;
 
-      this.container.appendChild(messageEl);
-      return messageEl;
+    this.narrator = {
+      treatAsCharacter: false,
+      italicize: true,
+      ...config.narrator
+    };
+
+    this.classes = {
+      name: "game-character-name",
+      message: "game-message",
+      menu: "game-menu",
+      button: "game-menu-button",
+      narrator: "game-narrator-message",
+      ...config.classes
+    }
+  }
+  
+  onNarratorMessage(message) {
+    const messageEl = document.createElement("div");
+    messageEl.className = this.classes.narrator;
+    
+    if (this.narrator.italicize) {
+      const italicEl = document.createElement("em");
+      italicEl.innerText = message;
+      messageEl.appendChild(italicEl);
+    } else {
+      messageEl.innerText = message;
     }
 
-    onMessage(character, message) {
-      if (character.id === this.game.narrator.id && !this.narrator.treatAsCharacter) {
-        return this.onNarratorMessage(message);
-      }
+    this.container.appendChild(messageEl);
+    return messageEl;
+  }
 
-      const messageEl = document.createElement("div");
-      messageEl.className = this.classes.message;
-
-      const nameEl = document.createElement("span");
-      nameEl.className = this.classes.name;
-      nameEl.innerText = character.name;
-
-      messageEl.append(nameEl, ": ", message);
-      this.container.appendChild(messageEl);
-      return messageEl;
+  onMessage(character, message) {
+    if (character.id === this.story.narrator.id && !this.narrator.treatAsCharacter) {
+      return this.onNarratorMessage(message);
     }
 
-    onMenu(options) {
+    const messageEl = document.createElement("div");
+    messageEl.className = this.classes.message;
+
+    const nameEl = document.createElement("span");
+    nameEl.className = this.classes.name;
+    nameEl.innerText = character.name;
+
+    messageEl.append(nameEl, ": ", message);
+    this.container.appendChild(messageEl);
+    return messageEl;
+  }
+
+  onMenu(options, onChoose) {
+    return new Promise(resolve => {
       const menuElement = document.createElement("div");
       menuElement.className = this.classes.menu;
 
-      options.forEach(option => {
+      for (let option in options) {
         const button = document.createElement("button");
         button.className = this.classes.button;
-        button.innerText = option.text;
+        button.innerText = option;
         button.onclick = () => {
-          option.action();
           this.container.removeChild(menuElement);
+          resolve(typeof options[option] === "function" ? options[option](menuElement) : options[option]);
         };
         menuElement.appendChild(button);
-      });
+      }
 
       this.container.appendChild(menuElement);
-      return menuElement;
-    }
+    });
   }
-	
-	class Game {
-    /**
-     * 
-     * @param {Object} config - Configuration object for the game.
-     * @param {Renderer | { type: Renderer, [key: string]: any }} config.renderer - Renderer instance to use for rendering the game.
-     * @param {Object} config.narrator - Configuration for the narrator character.
-     * @param {string} config.narrator.id - ID of the narrator character.
-     * @param {string} config.narrator.name - Name of the narrator character.
-     * @param {Function} config.onSceneChange - Callback function to be called when the scene changes.
-     * @param {Function} config.onFinish - Callback function to be called when the game finishes.
-     * @param {Function} config.onError - Callback function to handle errors.
-     */
-    constructor(config = {}) {
-      this.characters = {};
-      this.scenes = {};
+}
 
-      this.narrator = this.character(
-        config.narrator?.id || "narrator",
-        config.narrator?.name || "Narrator"
-      );
+/**
+ * @typedef {Record<string, unknown>} SednaCharacterConfig
+ * @prop {string?} name
+ */
 
-      this.config = config;
+/**
+ * @typedef {object} SednaConfig
+ * @prop {SednaRenderer | undefined} renderer - Renderer reference.
+ * @prop {SednaCharacterConfig | undefined} narrator - Narrator character configuration.
+ * @prop {Record<string, SednaCharacterConfig> | undefined} characters
+ * @prop {{ [id: string]: (ctx: Record<string, unknown>) => string | undefined }} scenes
+ * @prop {() => void | undefined} onSceneChange - Callback function to be called when the scene changes.
+ * @prop {() => void | undefined} onFinish - Callback function to be called when the game finishes.
+ * @prop {() => void | undefined} onError - Callback function to handle errors.
+ */
 
-      if (config.renderer instanceof Renderer) {
-        this.renderer = config.renderer;
-      }
-      else {
-        const RendererClass = config.renderer.type || HtmlRenderer;
-        if (typeof RendererClass !== "function" || !(RendererClass.prototype instanceof Renderer)) {
-          throw new Error("Invalid renderer type provided.");
-        }
-        this.renderer = new RendererClass(config.renderer);
-      }
+class Sedna {
+  /**
+   * @param {SednaConfig} config
+   */
+  constructor(config = {}) {
+    this.characters = {};
+    this.characterFunctions = {};
+    this.scenes = {};
 
-      this.renderer.insert(this);
+    this.data = {};
 
-      this.onSceneChange = config.onSceneChange;
-      this.onFinish = config.onFinish;
-      this.onError = config.onError || this._defaultErrorHandler;
-    }
+    this.renderer = config.renderer instanceof SednaRenderer ? config.renderer : new SednaHTMLRenderer(config.renderer);
+    this.renderer.setStory(this);
 
-    _defaultErrorHandler(error) {
-      throw new Error(error);
-    }
+    const { name: narratorName, ...narratorData } = config.narrator || { name: "Narrator" };
+    this.narrator = this.character("n", narratorName, narratorData);
 
-    /**
-     * Defines a new scene in the game.
-     * @param {string} label - Unique identifier for the scene.
-     * @param {Function} actions - Function that contains the actions for the scene.
-     */
-    scene(label, actions) {
-      this.scenes[label] = actions;
-    }
-
-    /**
-     * Creates a new character in the game.
-     * @param {string} id - Unique identifier for the character.
-     * @param {string} [name="???"] - Name of the character.
-     * @param {Object} [data={}] - Additional data associated with the character.
-     * @returns {Character} The created character instance.
-     */
-    character(id, name = "???", data = {}) {
-      const c = new Character(this, id, name, data);
-      this.characters[id] = c;
-      return c;
-    }
-
-    /**
-     * Prompts the user for a choice.
-     * @param {Array<{ text: string, action: Function }>} options - Array of menu options.
-     * @returns {HTMLElement} The menu element.
-     */
-    menu(options) {
-      if (!Array.isArray(options)) {
-        return this.onError("Menu options must be an array.");
-      }
-
-      const goto = this.goto.bind(this);
-
-      options = options.map(option => {
-        const _action = option.action;
-        option.action = () => {
-          const ret = _action();
-          if (typeof ret === "string") {
-            goto(ret);
-          }
-        };
-        return option;
-      });
-
-      return this.renderer.onMenu?.(options);
-    }
-
-    /**
-     * Navigates to a specific scene.
-     * @param {string} label - The label of the scene to navigate to.
-     */
-    goto(label) {
-      if (!this.scenes[label]) {
-        this.onError(`Scene "${label}" does not exist.`);
-        return;
-      }
-
-      const nextScene = this.scenes[label]();
-      if (typeof nextScene === "string") {
-        this.onSceneChange?.();
-        this.goto(nextScene);
+    if (config.characters) {
+      for (let id in config.characters) {
+        const { name, ...data } = config.characters[id];
+        this.character(id, name, data);
       }
     }
 
-    /**
-     * Starts the game by navigating to the "start" scene.
-     */
-    start() {
-      this.goto("start");
-      this.onFinish?.();
+    if (config.scenes) {
+      for (let id in config.scenes) {
+        this.scene(id, config.scenes[id]);
+      }
+    }
+
+    this.onSceneChange = config.onSceneChange;
+    this.onFinish = config.onFinish;
+    const defaultErrHandler = (err) => { throw new Error(err) };
+    this.onError = config.onError || defaultErrHandler;
+  }
+
+  /**
+   * Creates/overwrites a scene.
+   * @param {string} id - Unique scene identifier.
+   * @param {() => string | undefined} func - Scene function.
+   */
+  scene(id, func) {
+    this.scenes[id] = func;
+  }
+
+  /**
+   * Creates/overwrites a character.
+   * @param {string} id - Unique identifier for the character.
+   * @param {string} [name="???"] - Name of the character.
+   * @param {Object} [data={}] - Additional data associated with the character.
+   * @returns {SednaCharacter} The created character instance.
+   */
+  character(id, name = "???", data = {}) {
+    this.characters[id] = new SednaCharacter(this, id, name, data);
+    this.characterFunctions[id] = this.characters[id].asFunction();
+    return this.characters[id];
+  }
+
+  /**
+   * Prompts the user for a choice.
+   * @param {Record<string, () => Record | string | undefined>} options - Array of menu options.
+   */
+  async menu(options) {
+    const thenOption = options._;
+    delete options._;
+    const goto = this.goto.bind(this);
+    const returnValue = await this.renderer.onMenu?.(options);
+    if (thenOption && typeof thenOption === "function") {
+      await thenOption();
+    }
+    if (typeof returnValue === "string") {
+      await goto(returnValue);
+    }
+    else if (returnValue != undefined) {
+      this.onError(`Unknown type returned by menu option: ${returnValue}`);
     }
   }
 
-  const sedna = {
-    __VERSION: "1.0.0-dev",
+  /**
+   * Calls a scene and returns it's result. Doesn't affect the game flow.
+   * Use this for logic scenes.
+   * @param {string} id - Scene id.
+   */
+  async call(id) {
+    return await this.scenes[id]({ ...this.characterFunctions }, this.data);
+  }
 
-    Renderer,
-    HtmlRenderer,
-    
-    Character,
-    Game,
+  /**
+   * Navigates to a specific scene.
+   * @param {string} id - Scene id.
+   */
+  async goto(id) {
+    if (!this.scenes[id]) {
+      this.onError(`Scene "${id}" does not exist.`);
+      return;
+    }
 
-    game: (config = {}) => new Game(config)
-  };
+    const returnValue = await this.call(id);
+    // Goes to the next scene.
+    if (typeof returnValue === "string") {
+      this.onSceneChange?.();
+      await this.goto(returnValue);
+    }
+    // Possibly a menu
+    else if (returnValue) {
+      return await this.menu(returnValue);
+    }
+  }
 
-  window.sedna = sedna;
-})();
+  /**
+   * Starts the game by navigating to the "start" scene.
+   */
+  start() {
+    const onFinish = this.onFinish?.bind(this);
+    this.goto("start").then(() => onFinish?.());
+  }
+}
